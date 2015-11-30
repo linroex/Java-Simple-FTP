c939b89f22155ca6675fb9ce4343b6816520f82d
linroex <linroex@linroex-MacBook-Air.local>
2015-11-30 19:17:34 +0800

    init

---
+++ src/simpleftp/SimpleFTP.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package simpleftp;
+
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+/**
+ *
+ * @author linroex
+ */
+public class SimpleFTP {
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+    private class Client {
+    
+    }
+    
+    private class Server {
+        
+    }
+}
