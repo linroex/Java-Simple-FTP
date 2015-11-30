f781b288d80d1b9c704c236e4af403cfd9166f59
linroex <linroex@linroex-MacBook-Air.local>
2015-11-30 19:32:32 +0800

    server listen

--- src/simpleftp/SimpleFTP.java
+++ src/simpleftp/SimpleFTP.java
@@ -9,6 +9,7 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.IOException;
 /**
  *
  * @author linroex
@@ -27,6 +28,60 @@ public class SimpleFTP {
     }
     
     private class Server {
+        private final int port = 1234;
+        private ServerSocket server;
         
+        public Server() {
+            try {
+                this.server = new ServerSocket(this.port);
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
+            
+            this.listen();
+        }
+        
+        private void listen() {
+            while(true) {
+                try {
+                    Socket client = this.server.accept();
+                    
+                    System.out.println(client.getInetAddress() + " connected");
+                    
+                    Thread thread = new Thread(new ListenClientRunnable(client));
+                    thread.start();
+                } catch (IOException e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+        }
+        
+        private class ListenClientRunnable implements Runnable {
+            private DataInputStream input;
+            
+            public ListenClientRunnable(Socket client) {
+                try {
+                    this.input = new DataInputStream(client.getInputStream());
+                } catch(IOException e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+            
+            @Override
+            public void run() {
+                while(true) {
+                    try {
+                        String data = this.input.readUTF();
+
+                        System.out.println(data);
+                        
+                        // Identify command and do something.
+                    } catch (IOException e) {
+                        System.out.println(e.getMessage());
+                    }
+                    
+                }
+            }
+        }
     }
 }
